3b740ca90d965f26611200866fd7b4e2e01dd215
严荣兴 <yanrx20795@hundsun.com>
2018-05-20 20:41:38 +0800

    第一版发布

---
+++ src/main/java/com/better/xing/SelfBlogApplication.java
@@ -0,0 +1,14 @@
+package com.better.xing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SelfBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfBlogApplication.class, args);
+	}
+}
